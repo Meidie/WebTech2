37e5e37210e9568be8961b0316fed47087e5e41b
esi99 <45421291+esi99@users.noreply.github.com>
2019-05-12 19:03:45 +0200

    úprava na zhodu s uloha01
    úprava výzoru na zhodu s uloha01, pridanie session na admina, language

--- Uloha03/mailinfo.php
+++ Uloha03/mailinfo.php
@@ -4,7 +4,19 @@ session_start();
 <!DOCTYPE HTML>
 <html>
 <head>
+    <!-- Required meta tags -->
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" type="text/css" href="../Login/css/style.css">
+
+    <title><?php echo $language['title']?></title>
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -19,13 +31,51 @@ session_start();
 </head>
 <body>
 <header>
-    <h1 id="tit_mail">
+ <!--   <h1 id="tit_mail">
         Mail Info
     </h1>
     <nav>
         <ul class="menu">
             <li class="menu"><a href="index.php">Upload csv</a></li>
             <li class="menu"><a href="mailinfo.php">Mail info</a></li>
+        </ul>
+    </nav> -->
+
+    <nav class="navbar navbar-expand-md navbar-dark color-black">
+        <a class="navbar-brand" href="https://147.175.121.210:4171/files/SkuskoveZadanie/WebTech2/index.php?lang=<?php echo $language['websiteLang']?>"> <img height="60"  alt="logo" src="../Login/img/logo.png"> </a>
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <!--<h1 class="text-white"></h1>-->
+            </li>
+            <li class="navbar-nav mr-auto">
+                <a class="nav-link" href="index.php?lang=<?php echo $language['websiteLang']?>"><?php echo $language['profile']?>Upload csv</a>
+            </li>
+            <li class="navbar-nav mr-auto active">
+                <a class="nav-link" href="mailinfo.php?lang=<?php echo $language['websiteLang']?>"><?php echo $language['results']?>Mail info</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+            <li class="navbar-item">
+                <div id="skDiv"> <a class="nav-link" id="svk" href="index.php?lang=sk"> <img src="../Login/img/sk.png" height="30" alt="sk"></a></div>
+                <div id="enDiv" ><a class="nav-link" id="eng" href="index.php?lang=en"> <img src="../Login/img/uk.png" height="30" alt="uk"></a></div>
+                <?php
+
+                //vykreslenie spravnej vlajky
+                if(isset($_GET['lang']) && $_GET['lang'] == 'sk'){
+
+                    echo '<script>document.getElementById("skDiv").style.display = "none";</script>';
+                    echo '<script>document.getElementById("enDiv").style.display = "";</script>';
+                }else if(isset($_GET['lang']) && $_GET['lang'] == 'en'){
+
+                    echo '<script>document.getElementById("skDiv").style.display = "";</script>';
+                    echo '<script>document.getElementById("enDiv").style.display = "none";</script>';
+                }else{
+
+                    echo '<script>document.getElementById("skDiv").style.display = "none";</script>';
+                    echo '<script>document.getElementById("enDiv").style.display = "";</script>';
+                }
+                ?>
+            </li>
         </ul>
     </nav>
 
