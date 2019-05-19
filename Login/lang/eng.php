0dd007fe920979ca2b9cc260a8998a81bed9e925
Matúš Pohančenik <41523530+Meidie@users.noreply.github.com>
2019-05-19 20:05:36 +0200

    edit

---
+++ Login/lang/eng.php
@@ -0,0 +1,14 @@
+<?php
+
+    return array(
+        'websiteLang' => 'en',
+        'title' => 'Sign in',
+        'h1' => 'INFORMATION PORTAL',
+        'h2' => 'Sign in',
+        'name' => 'Username',
+        'password' => 'Password',
+        'loginButton' => 'Sign in',
+        'failedLogin' => 'Incorrect login information!',
+
+    )
+?>
