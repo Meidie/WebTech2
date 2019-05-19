3597462a1b2e4939a758f02127f268cc2699a7ce
esi99 <45421291+esi99@users.noreply.github.com>
2019-05-19 20:55:44 +0200

    Add files via upload

---
+++ Uloha03/lang/svk.php
@@ -0,0 +1,32 @@
+<?php
+
+return array(
+    'websiteLang' => 'sk',
+    'logout'=>'Odhlásiť',
+    'profile' => 'Profil',
+    'results' => 'Výsledky',
+    'point' => 'Body',
+    'generated' => 'Načítanie údajov s generovaním hesla',
+    'separator' => 'Oddeľovač',
+    'download' => 'Stiahnuť upravený súbor',
+    'nongenerated' => 'Načítanie údajov bez generovania hesla',
+    'upload' => 'Nahrať súbor',
+    'send' => 'Odoslanie údajov mailom',
+    'template' => 'Šablóna',
+    'use_temp' =>'Použi šablónu',
+    'sender' =>'Odosielateľ:',
+    'pw_mail' =>'Heslo na email:',
+    'name' =>'Meno a Priezvisko:',
+    'name2'=>'Meno Priezvisko',
+    'subject'=>'Predmet:',
+    'message' =>'Správa:',
+    'submit' =>'Odošli mail',
+    'date' => 'Dátum odoslania',
+    'recipient' => 'Príjemca',
+    'subject2' =>'Predmet',
+    'temp_id' => 'ID šablóny',
+    'mailinfo'=> 'Mail Info',
+    'mail' => 'Mail'
+)
+
+?>
