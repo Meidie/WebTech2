a66ae0e20b1acb6ddbaec161cbf87168e0172f59
Matus Pohancenik <matuspohancenik@gmail.com>
2019-05-19 17:57:27 +0000

    edit

--- Uloha01/php/config.php
+++ Uloha01/php/config.php
@@ -1,6 +1,9 @@
 <?php
 
     $hostname ="localhost";
+   // $username = "xpohancenikm";
+   // $password ="MLW8YNk14VSv";
+
     $username = "xmacakn";
     $password ="Heslo12345";
     $dbname ="skuskove_zadanie";
